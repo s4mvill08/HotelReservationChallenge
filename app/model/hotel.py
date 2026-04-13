8240f46977a9d14adc127c89b749a2107e9a4842
b14s210 <miguelypkr@gmail.com>
2026-04-13 17:04:02 -0500

    Primeras dos clases, guest and reservation con sus respectivos metodos atributos.

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -3,17 +3,49 @@ from datetime import datetime, date, timedelta
 from typing import ClassVar
 
 from app.services.util import (generate_unique_id, date_lower_than_today_error,
-    reservation_not_found_error, guest_not_found_error, room_not_available_error,
-    room_not_found_error, room_already_exists_error)
+                               reservation_not_found_error, guest_not_found_error, room_not_available_error,
+                               room_not_found_error, room_already_exists_error)
 
 
 # TODO: Implement Guest class here
+@dataclass
+class Guest:
+    RESULTADO: ClassVar[str] = "resultado"
+    VIP: ClassVar[str] =  "vip"
+    name: str
+    email: str
+    type_: str
+
+    def __str__(self) -> str:
+        pass
+
+    # TODO: Implement Reservation class here
 
 
-# TODO: Implement Reservation class here
+@dataclass
+class Reservation:
+    guest_name: str
+    description: str
+    check_in: date
+    check_out: date
+    id: str
+    guests: list[Guest]
 
+
+    def add_guest(self, name: str, email: str, type_: str) -> str:
+        pass
+
+    def delete_guest(self, guest_index) -> int:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+    def __len__(self) -> int:
+        pass
 
 # TODO: Implement Room class here
 
 
 # TODO: Implement Hotel class here
+
